296d815cb19e4b85f4f9c8ed448977d9d57bbe54
abdullah01 <arfathossen541@gmail.com>
2023-11-27 15:16:28 +0600

    crate useAuth and userole hooks for my work purpose

---
+++ src/Hooks/useRole.jsx
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { getRole } from "../Api/auth";
+import useAuth from "./useAuth";
+const useRole = () => {
+  const { user, loading } = useAuth();
+  const { data: role, isLoading } = useQuery({
+    enabled: !loading && !!user?.email,
+    queryFn: async () => await getRole(user?.email),
+    queryKey: ["role"],
+  });
+  return [role, isLoading];
+};
+
+export default useRole;
