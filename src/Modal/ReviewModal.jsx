846de6f384e204f870a58ef52601bf2b0775d5cb
abdullah01 <arfathossen541@gmail.com>
2023-11-27 14:54:10 +0600

    Let's create some modal components to dislplay modal on differnts pages

---
+++ src/Modal/ReviewModal.jsx
@@ -0,0 +1,131 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { addReview } from "../../Api/properties";
+import toast from "react-hot-toast";
+import useAuth from "../../Hooks/useAuth";
+const ReviewModal = ({ agent, title, closeModal, isOpen }) => {
+  const [loading, setLoading] = useState(false);
+  const { user } = useAuth();
+  const reviewerName = user?.displayName;
+  const reviewerImage = user?.photoURL;
+  const agentName = agent?.name;
+  const reviewDate = new Date();
+  const handleSubmit = async (e) => {
+    const reviewDescription = e.target.description.value;
+    if (reviewDescription.length < 10) {
+      toast.error("Please fill out minimum 10 characters ");
+    }
+    if (reviewDescription.length >= 10) {
+      closeModal();
+    }
+    const reviewData = {
+      propertyTitle: title,
+      reviewDescription,
+      reviewerName,
+      reviewerImage,
+      agentName,
+      reviewDate,
+    };
+    setLoading(true);
+    e.preventDefault();
+    try {
+      //   const data = await addReview(reviewData);
+      //   console.log(data);
+    } catch (err) {
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+    console.log(reviewData);
+  };
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Payment successful
+                </Dialog.Title>
+                <form onSubmit={handleSubmit}>
+                  <div className="mt-2">
+                    <label htmlFor="location" className="block text-gray-600">
+                      Property Title
+                    </label>
+                    <input
+                      className="w-full px-4 py-3 text-gray-800 border border-[#1c4456] focus:outline-[#1c4456] rounded-md "
+                      name="title"
+                      id="title"
+                      type="text"
+                      defaultValue={title}
+                      disabled
+                    />
+                    <div className="space-y-1 text-sm">
+                      <label
+                        htmlFor="description"
+                        className="block text-gray-600"
+                      >
+                        Description
+                      </label>
+
+                      <textarea
+                        id="description"
+                        className="block rounded-md focus:[#1c4456] w-full h-32 px-4 py-3 text-gray-800  border border-[#1c4456] focus:outline-[#1c4456] "
+                        name="description"
+                        required
+                        placeholder="Minimum 10 characters "
+                      ></textarea>
+                    </div>
+                  </div>
+
+                  <div className="mt-4 flex gap-3">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                    >
+                      Send
+                    </button>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+export default ReviewModal;
